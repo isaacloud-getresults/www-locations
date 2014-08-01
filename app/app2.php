01e7d1354b459559bc0f479b4fadb77d2f8a3b57
agolebiowska <golebiowska.agata@gmail.com>
2014-08-01 13:06:06 +0200

    typo

--- app/app2.php
+++ app/app2.php
@@ -43,7 +43,7 @@ if (isset($_GET['code']))
 	$gClient->authenticate($_GET['code']);
 	$_SESSION['token'] = $gClient->getAccessToken();
 
-	header('Location: hhttp://getresults.isaacloud.com/' );	
+	header('http://getresults.isaacloud.com/' );	
 	return;
 }
 

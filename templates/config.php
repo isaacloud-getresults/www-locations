87c2202e304a86a694390d15d85dee367f208d9a
agolebiowska <golebiowska.agata@gmail.com>
2014-08-20 09:06:19 +0200

    json unescaped slashes fix

--- templates/config.php
+++ templates/config.php
@@ -1,13 +1,10 @@
 <?php
 
 
-$sub = array_shift(explode(".",$_SERVER['SERVER_NAME']));  
+  $sub = array_shift(explode(".",$_SERVER['SERVER_NAME']));  
 
-//$sub="xd";
-
-$jsonString = file_get_contents('config.json');
-$data = json_decode($jsonString,true);
-
+  $jsonString = file_get_contents('config.json');
+  $data = json_decode($jsonString,true);
 
 
     $m = new MongoClient(); 
@@ -18,38 +15,22 @@ $data = json_decode($jsonString,true);
     $cursor = $collection->findOne(array( 'domain' => $sub ));
    
 
-    if(!empty($cursor))   
-	{     	
-
- 	
- 	//to zrobic w jednym przypisaniu
- 	//jesli ktorekolwiek jest nullem wyswietlic warning
- 	
- 	$base64 = $cursor['base64'];
-    $uuid= $cursor['uuid'];
+    if(!empty($cursor))      //check if instance exists
+	     {     	
+ 
+ 			$base64 = $cursor['base64'];
+   			$uuid= $cursor['uuid'];
  		      		
 
+		    $data['base64'][0] = $base64;
+   			$data['UUID'][0] = $uuid;
 
 
-   $data['base64'][0] = $base64;
-   $data['UUID'][0] = $uuid;
+			$newJsonString = json_encode($data, JSON_UNESCAPED_SLASHES);
+			file_put_contents('config.json', $newJsonString);
 
+         }
 
-
-
-
-$newJsonString = json_encode($data);
-file_put_contents('config.json', $newJsonString);
-
-
-
-
-}
-
-
-else
-
-echo "Error. Instance doesn't exist";
 
 
 ?>

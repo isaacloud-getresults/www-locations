6794a5d1fff44d514dcccf9e8411f88c8e07e101
agolebiowska <golebiowska.agata@gmail.com>
2014-08-26 11:41:13 +0200

    progress bar for location adding

--- templates/add.php
+++ templates/add.php
@@ -1,3 +1,10 @@
+
+       <link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
+  <link rel="stylesheet" href="/resources/demos/style.css"> 
+
+
 	<script type="text/javascript">
 			function disable(){
 				if(document.getElementById('nazwa').options[document.getElementById('nazwa').selectedIndex].value =='add')
@@ -8,15 +15,63 @@
 				document.getElementById('text2').disabled=true;}
 			
 			}
+			
+			
+			
+			
+		function showPleaseWait() {
+			var butt = document.getElementById("msgDiv");
+			butt.innerHTML="Please wait while your location is being added. It may take a while.";
+			 document.getElementById('progressbar').style.display = "";
+		 return true;
+		}
+  
+  
+  $(function() {
+$("#progressbar").progressbar({ value: 0 });
+setTimeout(updateProgress, 1000);
+});
+
+function updateProgress() {
+  var progress;
+  progress = $("#progressbar")
+    .progressbar("option","value");
+  if (progress < 100) {
+      $("#progressbar")
+        .progressbar("option", "value", progress + 1);
+      setTimeout(updateProgress, 1000);
+  }
+}
+
+
+
+	
+			
+			
+			
+			
 		</script>
 
 
-
+<style>
+  #progressbar {
+    margin-top: 20px;
+  }
+ 
+  .progress-label {
+    font-weight: bold;
+    text-shadow: 1px 1px 0 #fff;
+  }
+ 
+  .ui-dialog-titlebar-close {
+    display: none;
+  }
+  </style>
 
 <div class= "continer" style="width:600px" >
 
     	<h3><center>Add/delete location:</center></h3>	<br>
-	<form action="./add" method="POST" >
+	<form action="./add" method="POST" name="myForm" onSubmit="return showPleaseWait()">
 	
 	<dl class="dl-horizontal">
 	<dt>Option: </dt>
@@ -45,7 +100,23 @@
 		<br>	
 	<dl>
 	<center><button type="submit" name="execute" value="ok" class="btn btn-primary btn-lg"><font color="white">
-Execute</font></button></center>
-	
+OK</font></button></center>
 	</form>
+	
+	
+	
+	 </br></br>  	
+      <center>   	
+	<div id="msgDiv"></div></br></br>
+	
+	<div id="progressbar" style="display: none;"></div> 
+	
+	</center>
+         		
+        </div>
+	
+	
+	
+	
+	
 </div>
